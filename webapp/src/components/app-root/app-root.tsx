427b6753883c229905c8c1e321d324edd63f583d
DanielHabenicht <daniel-habenicht@outlook.de>
2022-07-29 01:02:05 +0000

    working?

--- webapp/src/components/app-root/app-root.tsx
+++ webapp/src/components/app-root/app-root.tsx
@@ -15,27 +15,32 @@ export class AppRoot {
   @State() result;
   @State() _model;
 
+  @State() labels = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+
   componentWillLoad() {
-    this._model = tf.sequential();
-    this._model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
-
-    this._model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-
-    // Generate some synthetic data for training.
-    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-    const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-    // Train the this._model using the data.
-    this._model.fit(xs, ys, { epochs: 10 }).then(() => {
-      // Use the model to do inference on a data point the model hasn't seen before:
-      // Open the browser devtools to see the output
+    // @ts-ignore
+    // this._model = tf.loadLayersModel('assets/model.json');
+    tf.loadLayersModel('assets/model.json').then(model => {
+      this._model = model;
+      console.log('model loaded');
     });
+    // this._model = tf.sequential();
+    // this._model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+    // this._model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+    // // Generate some synthetic data for training.
+    // const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+    // const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+    // // Train the this._model using the data.
+    // this._model.fit(xs, ys, { epochs: 10 }).then(() => {
+    //   // Use the model to do inference on a data point the model hasn't seen before:
+    //   // Open the browser devtools to see the output
+    // });
   }
 
   componentDidLoad() {
-    // tf.loadLayersModel('jsmodel/model.json').then(model => {
-    //   this._model = model;
-    // });
     if (this.cam != undefined) {
       this.cam.start(1);
       this.cam.addEventListener('picture', e => {
@@ -44,19 +49,23 @@ export class AppRoot {
         var i = new Image();
 
         i.onload = () => {
-                this.picture_height = i.width;
-                this.picture_width = i.height;
-                console.log(this.picture_height, this.picture_width)
-        };
+            this.picture_height = i.width;
+            this.picture_width = i.height;
+            console.log(this.picture_height, this.picture_width);
+    
+            var test = tf.browser.fromPixels(i).resizeBilinear([180, 180]);
+            console.log(test.shape);
+            const offset = tf.scalar(255.0);
+            const normalized = tf.expandDims(test.div(offset), 0);
+
+            var score = tf.softmax(tf.tensor(this._model.predict(normalized).arraySync()[0]));
+            var confidence = tf.max(score).dataSync()[0];
+            this.result = this.labels[tf.argMax(score).dataSync()[0]] + " with " + confidence;
+          };
 
         i.src = e.detail;
-        // @ts-ignore
-        this._model.predict(tf.tensor2d([5], [1, 1])).print();
-        this.result = this._model
-          .predict(tf.tensor2d([5], [1, 1]))
-          .as1D()
-          .argMax()
-          .dataSync()[0];
+        i.crossOrigin = 'anonymous';
+        // i.src = 'https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg';
 
         this.cam.stop();
       });
